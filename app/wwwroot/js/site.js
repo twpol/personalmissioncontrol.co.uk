794b67d97d4400c3bdbda181ac639028b2bc5b49
James Ross <git@james-ross.co.uk>
2022-01-08 20:57:10 +0000

    feat: Add shortcut keys for flagging and completing email messages

--- app/wwwroot/js/site.js
+++ app/wwwroot/js/site.js
@@ -52,23 +52,32 @@ function messageHighlightScroll() {
   }
 }
 
+const apiStatus = new URL(
+  `/api/${location.pathname.split("/")[1]}/email/status`,
+  location
+);
+
+function setMessageStatus(message, field, value) {
+  if (value === true) value = "True";
+  if (value === false) value = "False";
+  apiStatus.search = new URLSearchParams([
+    ["id", message.dataset.id],
+    [field, value],
+  ]).toString();
+  fetch(apiStatus, { method: "POST" });
+  message.dataset[field] = value;
+  if (field === "flagged" && value) message.dataset.completed = "False";
+  if (field === "completed" && value) message.dataset.flagged = "False";
+}
+
 function messageUpdateUnreadScroll() {
-  const api = new URL(
-    `/api/${location.pathname.split("/")[1]}/email/status`,
-    location
-  );
   const currentMessage = document.querySelector("ul.messages > li.current");
   let unread = false;
   for (const message of document.querySelectorAll("ul.messages > li")) {
     if (message === currentMessage) unread = true;
     const messageUnread = message.dataset.unread === "True";
     if (messageUnread !== unread) {
-      api.search = new URLSearchParams([
-        ["id", message.dataset.id],
-        ["unread", unread],
-      ]).toString();
-      fetch(api, { method: "POST" });
-      message.dataset.unread = unread ? "True" : "False";
+      setMessageStatus(message, "unread", unread);
     } else if (unread) {
       break;
     }
@@ -80,11 +89,41 @@ const messageUpdateUnreadScrollDBL = debounceLazy(
   messageUpdateUnreadScroll
 );
 
+function messageKeyDown(event) {
+  const currentMessage = document.querySelector("ul.messages > li.current");
+  if (!currentMessage) return;
+  const key = [
+    event.altKey ? "Alt+" : "",
+    event.ctrlKey ? "Control+" : "",
+    event.shiftKey ? "Shift+" : "",
+    event.metaKey ? "Meta+" : "",
+    event.code,
+  ].join("");
+  const flagged = currentMessage.dataset.flagged === "True";
+  const completed = currentMessage.dataset.completed === "True";
+  switch (key) {
+    case "Insert":
+      if (!completed) {
+        setMessageStatus(currentMessage, "flagged", !flagged);
+      }
+      break;
+    case "Control+Insert":
+      if (flagged) {
+        setMessageStatus(currentMessage, "completed", true);
+      } else if (completed) {
+        setMessageStatus(currentMessage, "flagged", true);
+      }
+      break;
+  }
+}
+
 if (document.querySelector("ul.messages")) {
   window.addEventListener("load", function () {
     // Delay scroll by a tiny fraction so we override the browser's build in scroll position restoration
     setTimeout(messageAutoScroll, 100);
   });
+
+  window.addEventListener("keydown", messageKeyDown);
 }
 
 function debounce(callback) {
