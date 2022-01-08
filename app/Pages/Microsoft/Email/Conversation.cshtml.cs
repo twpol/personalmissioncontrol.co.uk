02372163a37bdc78ab4e1500016c01aec43b72f4
James Ross <git@james-ross.co.uk>
2022-01-08 13:14:52 +0000

    feat: Add page listing Microsoft email conversations and messages in conversations

---
+++ app/Pages/Microsoft/Email/Conversation.cshtml.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using app.Auth;
+using Microsoft.Graph;
+
+namespace app.Pages.Microsoft.Email
+{
+    public class ConversationModel : MicrosoftPageModel
+    {
+        public const string HtmlBodyPrefix = @"
+            <style>
+                body {
+                    margin: 1rem;
+                }
+                img {
+                    max-width: 100%;
+                }
+            </style>
+        ";
+        public const string HtmlBodyPostfix = "";
+
+        public IEnumerable<DisplayMessage> Messages;
+        public string ConversationName;
+
+        GraphServiceClient Graph;
+
+        public ConversationModel(MicrosoftGraphProvider graphProvider)
+        {
+            Graph = graphProvider.Client;
+        }
+
+        public async Task OnGet(string conversation)
+        {
+            var message = await Graph.Me.Messages.Request()
+                .Filter($"conversationId eq '{conversation}'")
+                .Top(1)
+                .Select(message => new
+                {
+                    message.Subject,
+                })
+                .GetAsync();
+            ConversationName = CleanSubject(message.First().Subject);
+
+            Messages = (await GetAllPages(Graph.Me.Messages.Request()
+                .Filter($"conversationId eq '{conversation}'")
+                .Top(1000)
+                .Select(message => new
+                {
+                    message.SentDateTime,
+                    message.From,
+                    message.ToRecipients,
+                    message.IsRead,
+                    message.Flag,
+                    message.Body,
+                })))
+                .OrderBy(message => message.SentDateTime)
+                .Select(message => new DisplayMessage(
+                    message.SentDateTime ?? DateTimeOffset.MinValue,
+                    message.From.EmailAddress,
+                    message.ToRecipients.First().EmailAddress,
+                    message.IsRead == false,
+                    message.Flag.FlagStatus == FollowupFlagStatus.Flagged,
+                    message.Flag.FlagStatus == FollowupFlagStatus.Complete,
+                    message.Body.Content
+                ));
+        }
+
+        async Task<IList<Message>> GetAllPages(IUserMessagesCollectionRequest request)
+        {
+            var list = new List<Message>();
+            do
+            {
+                var messages = await request.GetAsync();
+                list.AddRange(messages);
+                request = messages.NextPageRequest;
+            } while (request != null);
+            return list;
+        }
+
+        string CleanSubject(string subject)
+        {
+            var length = 0;
+            do
+            {
+                length = subject.Length;
+                subject = subject.Trim();
+                if (subject.StartsWith("re:", StringComparison.OrdinalIgnoreCase)) subject = subject.Substring(3);
+                if (subject.StartsWith("fw:", StringComparison.OrdinalIgnoreCase)) subject = subject.Substring(3);
+                if (subject.StartsWith("fwd:", StringComparison.OrdinalIgnoreCase)) subject = subject.Substring(4);
+            } while (length != subject.Length);
+            return subject;
+        }
+
+        public record DisplayMessage(DateTimeOffset Date, EmailAddress From, EmailAddress To, bool Unread, bool Flagged, bool Completed, string HtmlBody)
+        {
+            public string HtmlBodyForIFrame
+            {
+                get
+                {
+                    return HtmlBodyPrefix + HtmlBody + HtmlBodyPostfix;
+                }
+            }
+        }
+    }
+}
--- app/wwwroot/js/site.js
+++ app/wwwroot/js/site.js
@@ -1,1 +1,18 @@
-﻿
+﻿function seamlessResize() {
+  const frames = document.querySelectorAll("iframe.seamless");
+  for (const frame of frames) {
+    frame.height = frame.contentDocument.documentElement.offsetHeight;
+  }
+}
+
+window.addEventListener("resize", seamlessResize);
+window.addEventListener("load", seamlessResize);
+
+function dateTimeLocal() {
+  const elements = document.querySelectorAll(".datetime-local[data-ts]");
+  for (const element of elements) {
+    element.textContent = new Date(element.dataset.ts).toLocaleString();
+  }
+}
+
+window.addEventListener("load", dateTimeLocal);
