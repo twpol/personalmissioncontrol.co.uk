a9b6d834ea5249d4d5507ea6190420f44a043f51
James Ross <git@james-ross.co.uk>
2024-08-08 21:55:43 +0100

    feat: Allow specifying parent ID in model store collections

--- app/Services/IModelStore.cs
+++ app/Services/IModelStore.cs
@@ -7,7 +7,7 @@ namespace app.Services
 {
     public interface IModelStore<T> where T : BaseModel
     {
-        public IAsyncEnumerable<T> GetCollectionAsync(string accountId);
-        public Task UpdateCollectionAsync(string accountId, Func<IAsyncEnumerable<T>> updater);
+        public IAsyncEnumerable<T> GetCollectionAsync(string accountId, string? parentId);
+        public Task UpdateCollectionAsync(string accountId, string? parentId, Func<IAsyncEnumerable<T>> updater);
     }
 }
